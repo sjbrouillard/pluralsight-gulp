b7014d8eade2824d37466d45e045154af882fd16
Stephen J. Brouillard <stephen.brouillard@egov.com>
2016-01-11 11:51:21 -0500

    Got rid of hello world task and added initial vet task for code analysis

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
 
-gulp.task('hello-world', function(){
-    console.log('Our first hello world task!');
+gulp.task('vet', function() {
+    return gulp.src([
+        './src/**/*.js',
+        './*.js'
+    ])
+    .pipe(jscs())
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish', {verbose: true}));
 });
