2c99a39edc43289af977f20db298f399cfae17db
Stephen J. Brouillard <stephen.brouillard@egov.com>
2016-01-11 10:46:10 -0500

    Added linting packages and typing for angular

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -3,3 +3,5 @@
 /// <reference path="node/node.d.ts" />
 /// <reference path="orchestrator/orchestrator.d.ts" />
 /// <reference path="q/Q.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
