cbcde0b37508b0eb788ff3ba30d019cf9e48e521
Stephen J. Brouillard <stephen.brouillard@egov.com>
2016-01-12 09:25:29 -0500

    Minor format changes

--- gulpfile.js
+++ gulpfile.js
@@ -6,13 +6,15 @@ var $ = require('gulp-load-plugins') ({lazy: true});
 
 gulp.task('vet', function() {
     log('Analyzing source code with JSHint and JSCS');
-    return gulp.src(config.alljsfiles)
-    .pipe($.if(args.verbose,$.print()))
-    .pipe($.jscs())
-    .pipe($.jscs.reporter())
-    .pipe($.jshint())
-    //If you want CI to fail the build, just use .pipe(jshint.reporter('fail))
-    .pipe($.jshint.reporter('jshint-stylish', {verbose: true}));
+
+    return gulp
+        .src(config.alljsfiles)
+        .pipe($.if(args.verbose,$.print()))
+        .pipe($.jscs())
+        .pipe($.jscs.reporter())
+        .pipe($.jshint())
+        //If you want CI to fail the build, just use .pipe(jshint.reporter('fail'))
+        .pipe($.jshint.reporter('jshint-stylish', {verbose: true}));
 });
 
 /////////////////
